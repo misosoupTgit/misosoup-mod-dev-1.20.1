d049193ab31d07170fde86077660d17945af2c85
みそすーぷ <ahobaka1107@outlook.com>
2025-07-23 00:49:51 +0900

    Add files via upload

---
+++ src/main/java/com/github/misosoupTgit/examplemod/ExampleMod.java
@@ -0,0 +1,51 @@
+// パッケージ名は、githubのドメインを逆順に+mod名で
+// 例 com.github.misosoupTgit.examplemod 変更するのはexamplemodのみでok
+package com.github.misosoupTgit.examplemod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ExampleMod.MOD_ID)
+public class ExampleMod {
+    // modidの設定※クラス名も変更 shift+f6で一括変更可能
+    public static final String MOD_ID = "examplemod";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ExampleMod(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
